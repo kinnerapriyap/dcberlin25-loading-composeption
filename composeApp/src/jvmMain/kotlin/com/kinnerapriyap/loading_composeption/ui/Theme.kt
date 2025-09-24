8e87c0189f08d5c25b51f799571ef44fdf48502d
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2025-09-24 11:40:45 +0200

    Set window size

--- composeApp/src/jvmMain/kotlin/com/kinnerapriyap/loading_composeption/ui/Theme.kt
+++ composeApp/src/jvmMain/kotlin/com/kinnerapriyap/loading_composeption/ui/Theme.kt
@@ -8,8 +8,8 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.platform.Font
@@ -45,6 +45,8 @@ private val DarkColors = darkColorScheme(
     onPrimary = Color(0xFF212121),
     onSecondary = Color(0xFF757575)
 )
+
+val LocalWindowSize = staticCompositionLocalOf { DpSize.Zero }
 
 @Composable
 fun DroidconBerlinTheme(
@@ -84,7 +86,7 @@ fun DroidconBerlinTheme(
         labelSmall = defaultTypography.labelSmall.copy(fontFamily = droidconBerlinFontFamily)
     )
     CompositionLocalProvider(
-        LocalWindowInfo provides windowSize,
+        LocalWindowSize provides windowSize,
     ) {
         MaterialTheme(
             colorScheme = colors,
@@ -93,4 +95,3 @@ fun DroidconBerlinTheme(
         )
     }
 }
-
